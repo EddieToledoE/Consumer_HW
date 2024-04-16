274d5e64bbda83e676df55bd8c6cd9be6784bbdb
EddieToledoE <teddy_03@outlook.es>
2024-04-16 12:32:26 -0600

    always active

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mqtt = require("mqtt");
 const io = require("socket.io-client");
 const dote = require("dotenv");
 dote.config();
+
 const brokerAddress = process.env.MQTT_BROKER_URL || "mqtt://localhost:1883";
 const topicData = process.env.TOPIC_DATA || "data";
 const socket = io(process.env.SOCKET_SERVER_URL, { transports: ["websocket"] });
@@ -10,25 +11,37 @@ const sendIncomingData = (data) => {
   socket.emit("IncomingData", data);
 };
 
-const client = mqtt.connect(brokerAddress);
+let client;
 
-client.on("error", (err) => {
-  console.error("Connection error:", err);
-  process.exit(1);
-});
+try {
+  client = mqtt.connect(brokerAddress);
+} catch (error) {
+  console.error("Error connecting to MQTT broker:", error.message);
+}
 
-client.on("connect", () => {
-  console.log("Connected to MQTT broker");
-
-  client.subscribe(topicData, { qos: 1 });
-  console.log(`Subscribed to topic: ${topicData}`);
-});
-
-client.on("message", (topic, message) => {
-  console.log(`Received message from topic: ${topic}`);
-  console.log(`Message: ${message.toString()}`);
-  sendIncomingData({
-    topic: topic,
-    message: message.toString(),
+if (client) {
+  client.on("error", (err) => {
+    console.error("Connection error:", err);
   });
-});
+
+  client.on("connect", () => {
+    console.log("Connected to MQTT broker");
+
+    client.subscribe(topicData, { qos: 1 }, (err) => {
+      if (err) {
+        console.error("Error subscribing to topic:", err);
+      } else {
+        console.log(`Subscribed to topic: ${topicData}`);
+      }
+    });
+  });
+
+  client.on("message", (topic, message) => {
+    console.log(`Received message from topic: ${topic}`);
+    console.log(`Message: ${message.toString()}`);
+    sendIncomingData({
+      topic: topic,
+      message: message.toString(),
+    });
+  });
+}
